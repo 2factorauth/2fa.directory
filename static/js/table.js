f73932c04757cd5481068031b1762e420549ffc1
Carlgo11 <carlgo11@users.noreply.github.com>
2022-10-16 01:19:34 +0200

    Add bottom border to active btn

--- static/js/table.js
+++ static/js/table.js
@@ -13,6 +13,7 @@ $('.category-btn').click(function () {
     const href = $(this).attr('href');
     if (window.location.hash === href) {
         history.pushState("", document.title, window.location.pathname + window.location.search);
+        $(`.category-btn.active`).removeClass('active');
         $('.table').collapse('hide');
     } else {
         window.location.hash = href;
@@ -20,13 +21,14 @@ $('.category-btn').click(function () {
 });
 
 function showCategory(category) {
-    $(`.table:not([data-table='${category}'])`).collapse('hide');
     const table = $(`.table[data-table='${category}']`);
     const button = $(`.category-btn[href^="#${category}"]`)
     const index = button.index('.category-btn');
     const length = $('.categories').css('grid-template-columns').split(' ').length;
     const row = Math.floor(index / length);
-    const column = index % length;
-    $(`.table[data-table='${category}']`).attr('style', `grid-area: ${row + 2} / 1 / ${row + 3} / ${length + 1}`);
+    $(`.table:not([data-table='${category}'])`).collapse('hide');
+    table.attr('style', `grid-area: ${row + 2} / 1 / ${row + 3} / ${length + 1}`);
     table.collapse('show');
+    $(`.category-btn:not([href^="#${category}"])`).removeClass('active');
+    $(button).addClass('active');
 }
