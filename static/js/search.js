2becf6dea5e67229804ad2b7007e6add75a5c1b9
Carlgo11 <carlgo11@users.noreply.github.com>
2022-10-18 05:24:14 +0200

    Add Algolia InstantSearch

--- static/js/search.js
+++ static/js/search.js
@@ -1,8 +1,44 @@
-$.ajax({
-    async: true,
-    crossDomain: true,
-    url: '/functions/v4/',
-    method: 'GET',
-    headers: {'Authorization':'b7a5116d-bfcb-425f-ac32-74e6fd989885'},
-    data: {}
+// Connect and authenticate with your Algolia app
+const client = algoliasearch('9TTZ08M03E', 'faa905e846fea4f08548776bc7273855')
+
+// Create a new index and add a record
+const index = client.initIndex('2fa.directory')
+let timeout = null;
+
+$('.search').on('input', function () {
+    if (timeout) {
+        clearTimeout(timeout);
+    }
+    timeout = setTimeout(function () {
+        sendSearch($('.search').val()); //this is your existing function
+    }, 1000);
+
 })
+
+function sendSearch(query) {
+    $('.entry.hit').removeClass('hit');
+    if (query !== undefined || query !== "") {
+        index.search(query, {
+            attributesToRetrieve: ['objectID']
+        })
+            .then(({hits}) => {
+                hits.forEach((hit) => {
+                    const entry_name = hit['objectID'];
+                    const entry = $(`.entry[data-domain^='${entry_name}']`);
+                    entry.addClass('hit');
+                    const entry_parent = $(`.table > .entry[data-domain^='${entry_name}']`).parent();
+                    entry_parent.collapse('show');
+                })
+                $('.category-btn').parent().hide();
+                $('.table').filter(function() { return !($(this).children().is('.hit')); }).collapse('hide');
+                $('.categories').addClass('search-results');
+                $('.entry.hit').show();
+                $('.entry:not(.hit)').hide();
+            })
+    }else{
+        //TODO: Reset page?
+        $('.table').removeClass('search-results');
+        $('.entry').show();
+    }
+}
+
