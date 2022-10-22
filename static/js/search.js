a7ce97c1c6f47d356125cf375d9ab9604c698e60
Carlgo11 <carlgo11@users.noreply.github.com>
2022-10-23 01:17:41 +0200

    Rewrite search input CSS

--- static/js/search.js
+++ static/js/search.js
@@ -46,7 +46,6 @@ function sendSearch(query) {
                 // Hide all non-matching entries
                 $('.entry:not(.hit)').hide();
                 $('.entry.hit').show();
-
             })
     }
 }
