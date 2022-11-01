f61bb03b764783ffe1ceb2bf4706b6412de83e71
Carlgo11 <carlgo11@users.noreply.github.com>
2022-11-01 04:04:33 +0100

    Fetch all.json from live API

--- entries.rb
+++ entries.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'json'
 require 'fileutils'
+require 'net/http'
+require 'json'
 require 'yaml'
 require 'parallel'
+require 'uri'
 
 data_dir = './data'
-websites = JSON.parse(File.read("#{data_dir}/all.json"))
 categories = JSON.parse(File.read("#{data_dir}/categories.json"))
+url = URI('https://2fa.directory/api/v3/all.json')
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
+request = Net::HTTP::Get.new(url)
+response = https.request(request)
+websites = JSON.parse(response.body)
 
 Parallel.each(websites, in_threads: 8) do |name, website|
   website['keywords'].each do |category|
