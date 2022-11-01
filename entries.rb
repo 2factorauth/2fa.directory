c88733b203b4cfa8b40b94011e51bcfe27894bf0
Carlgo11 <carlgo11@users.noreply.github.com>
2022-11-01 04:24:27 +0100

    Fix entries.rb

--- entries.rb
+++ entries.rb
@@ -8,20 +8,18 @@ require 'yaml'
 require 'parallel'
 require 'uri'
 
-data_dir = './data'
-categories = JSON.parse(File.read("#{data_dir}/categories.json"))
+categories = {}
 url = URI('https://2fa.directory/api/v3/all.json')
 https = Net::HTTP.new(url.host, url.port)
 https.use_ssl = true
 request = Net::HTTP::Get.new(url)
 response = https.request(request)
-websites = JSON.parse(response.body)
 
-Parallel.each(websites, in_threads: 8) do |name, website|
+JSON.parse(response.body).each do |name, website|
   website['keywords'].each do |category|
     categories[category] = {} unless categories.key? category
     categories[category][name] = website
   end
 end
 
-File.open("#{data_dir}/entries.json", "w") { |file| file.write JSON.generate categories }
+File.open("./data/entries.json", "w") { |file| file.write JSON.generate categories }
