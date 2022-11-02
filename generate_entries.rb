2b64c64b238454ca090efe07f7456cafcd5b314f
Carlgo11 <carlgo11@users.noreply.github.com>
2022-11-02 23:26:00 +0100

    Generate regional content

--- generate_entries.rb
+++ generate_entries.rb
@@ -8,18 +8,18 @@ require 'yaml'
 require 'parallel'
 require 'uri'
 
-categories = {}
 url = URI('https://2fa.directory/api/v3/all.json')
 https = Net::HTTP.new(url.host, url.port)
 https.use_ssl = true
 request = Net::HTTP::Get.new(url)
 response = https.request(request)
+entries = {}
 
 JSON.parse(response.body).each do |name, website|
   website['keywords'].each do |category|
-    categories[category] = {} unless categories.key? category
-    categories[category][name] = website
+    entries[category] = {} unless entries.key? category
+    entries[category][name] = website
   end
 end
 
-File.open("./data/entries.json", "w") { |file| file.write JSON.generate categories }
+File.open("./data/entries.json", "w") { |file| file.write JSON.generate entries }
--- generate_region_sites.rb
+++ generate_region_sites.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'json'
 require 'fileutils'
-require 'yaml'
+require 'json'
+require 'net/http'
 require 'parallel'
+require 'uri'
+require 'yaml'
 
+# Generate /content/{region}/_index.md
 data_dir = './data'
 regions = YAML.load_file("#{data_dir}/regions.yml")
 regions.each do |region|
@@ -14,3 +17,31 @@ regions.each do |region|
   end
 end
 
+# Generate /data/{region}.json
+categories = JSON.parse(File.read('./data/categories.json'))
+regions = YAML.load_file('./data/regions.yml')
+url = URI('https://2fa.directory/api/v3/all.json')
+https = Net::HTTP.new(url.host, url.port)
+https.use_ssl = true
+request = Net::HTTP::Get.new(url)
+response = https.request(request)
+
+regions.each do |region|
+  used_categories = {}
+
+  JSON.parse(response.body).each do |_, entry|
+    unless entry['regions'].nil?
+      site_regions = entry['regions'].reject { |r| r.start_with?('-') }
+      site_excluded_regions = entry['regions'].select { |r| r.start_with?('-') }.map! { |region_code| region_code.tr('-', '') }
+    end
+
+    unless entry['regions'].nil? || site_regions.empty? || site_regions.include?(region['id']) || region['id'].eql?('int')
+      next
+    end
+
+    next if !site_excluded_regions.nil? && site_excluded_regions.include?(region['id'])
+
+    entry['keywords'].each { |category| used_categories[category] = categories[category] unless used_categories.key? category }
+  end
+  File.open("./data/#{region['id']}.json", 'w') { |file| file.write JSON.generate used_categories }
+end
