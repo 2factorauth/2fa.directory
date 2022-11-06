189055eb7c075f0bb17afe52ee4070b89dce5f84
Carlgo11 <carlgo11@users.noreply.github.com>
2022-11-06 02:49:27 +0100

    Add exceptions note

--- assets/js/table.js
+++ assets/js/table.js
@@ -1,54 +1,54 @@
 $(document).ready(() => {
-    // Show category of query
-    const query = window.location.hash;
-    if (query && query.indexOf('#') > -1) showCategory(query.substring(1));
+  // Show category of query
+  const query = window.location.hash;
+  if (query && query.indexOf('#') > -1) showCategory(query.substring(1));
 });
 
 $(window).on('hashchange', async () => {
-    const query = window.location.hash;
-    if (query && query.indexOf('#') > -1) await showCategory(query.substring(1));
+  const query = window.location.hash;
+  if (query && query.indexOf('#') > -1) await showCategory(query.substring(1));
 });
 
 $('.category-btn').click(function () {
-    const href = $(this).attr('href');
-    if (window.location.hash === href) {
-        history.pushState("", document.title, window.location.pathname + window.location.search);
-        $(`.category-btn.active`).removeClass('active');
-        $('.table').collapse('hide');
-    } else {
-        window.location.hash = href;
-    }
+  const href = $(this).attr('href');
+  if (window.location.hash === href) {
+    history.pushState("", document.title, window.location.pathname + window.location.search);
+    $(`.category-btn.active`).removeClass('active');
+    $('.table').collapse('hide');
+  } else {
+    window.location.hash = href;
+  }
 });
 
 function showCategory(category) {
-    const table = $(`.table[data-table='${category}']`);
-    const button = $(`.category-btn[href^="#${category}"]`)
-    const index = button.index('.category-btn');
-    const length = $('.categories').css('grid-template-columns').split(' ').length;
-    const row = Math.floor(index / length);
-    $(`.table:not([data-table='${category}'])`).collapse('hide');
-    table.attr('style', `grid-area: ${row + 2} / 1 / ${row + 3} / ${length + 1}`);
-    table.collapse('show');
-    $(`.category-btn:not([href^="#${category}"])`).removeClass('active');
-    $(button).addClass('active');
+  const table = $(`.table[data-table='${category}']`);
+  const button = $(`.category-btn[href^="#${category}"]`)
+  const index = button.index('.category-btn');
+  const length = $('.categories').css('grid-template-columns').split(' ').length;
+  const row = Math.floor(index / length);
+  $(`.table:not([data-table='${category}'])`).collapse('hide');
+  table.attr('style', `grid-area: ${row + 2} / 1 / ${row + 3} / ${length + 1}`);
+  table.collapse('show');
+  $(`.category-btn:not([href^="#${category}"])`).removeClass('active');
+  $(button).addClass('active');
 }
 
 // Initialise popovers
-const exceptionPopoverList = [...document.querySelectorAll('.exception')].map(el => new bootstrap.Popover(el, {
-    trigger: 'hover focus',
-    title: 'Exceptions & Restrictions'
+const exceptionPopoverList = [...document.querySelectorAll('.note')].map(el => new bootstrap.Popover(el, {
+  trigger: 'hover focus',
+  title: 'Exceptions & Restrictions'
 }));
 
 const customTfaPopoverConfig = {
-    html: true,
-    sanitize: false,
-    trigger: 'hover focus'
+  html: true,
+  sanitize: false,
+  trigger: 'hover focus'
 }
 const customHardwarePopoverList = [...document.querySelectorAll('.custom-hardware-popover')].map(el => new bootstrap.Popover(el, {
-    ...customTfaPopoverConfig,
-    title: 'Custom Hardware 2FA'
+  ...customTfaPopoverConfig,
+  title: 'Custom Hardware 2FA'
 }));
 const customSoftwarePopoverList = [...document.querySelectorAll('.custom-software-popover')].map(el => new bootstrap.Popover(el, {
-    ...customTfaPopoverConfig,
-    title: 'Custom Software 2FA'
+  ...customTfaPopoverConfig,
+  title: 'Custom Software 2FA'
 }));
