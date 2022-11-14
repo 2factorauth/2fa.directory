58063efee1e9b0626d58d58c9774fdfe07ed5679
Carlgo11 <carlgo11@users.noreply.github.com>
2022-11-14 01:35:00 +0100

    Remove unused JS code & only show dropdown for tables

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,9 +1,3 @@
-let resizeObserver = new ResizeObserver(() => {
-    if ($('body').height() < $(window).height()) $('footer').css({position: 'absolute'});
-    else $('footer').css({position: 'static'});
-  })
-;
-resizeObserver.observe($('body')[0]);
 $(document).ready(() => {
   if ('serviceWorker' in navigator) navigator.serviceWorker.register('/service-worker.js');
 })
