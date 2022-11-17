433dec00c341d7c3f30fac9879c93c961ed0c40c
Carlgo11 <carlgo11@users.noreply.github.com>
2022-11-17 19:38:54 +0100

    Add offline fallback page for PWA

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,11 +1,50 @@
-self.addEventListener('fetch', async event => {
-  event.respondWith(caches.open('dynamic-cache').then(async cache => {
-    return await fetch(event.request).then(async response => {
-      await cache.delete(event.request);
-      await cache.put(event.request, response.clone());
-      return response;
-    }).catch(() => {
-      return cache.match(event.request);
-    });
-  }));
+const offline_page = '/offline/';
+const cachedPaths = ['script', 'css'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil((async () => {
+    console.log('[Service Worker] Install');
+    const cache = await caches.open('pwa-assets');
+    await cache.add(new Request(offline_page, {cache: 'reload'}));
+  })());
+
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil((async () => {
+    // Enable navigation preload if it's supported.
+    // See https://developers.google.com/web/updates/2017/02/navigation-preload
+    if ('navigationPreload' in self.registration) await self.registration.navigationPreload.enable();
+  })());
+
+  // Tell the active service worker to take control of the page immediately.
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    try {
+      if (cachedPaths.includes(e.request.destination) || e.request.mode === 'navigate') {
+        const r = await caches.match(e.request);
+        console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+        if (r) {
+          console.log(`Cache hit`);
+          return r
+        }
+
+        const response = await fetch(e.request);
+        const cache = await caches.open('pwa-assets');
+        console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+        cache.put(e.request, response.clone());
+        return response;
+      } else return await fetch(e.request);
+    } catch (error) {
+      console.log(`ERROR: ${error}`);
+      if (e.request.mode === 'navigate') {
+        const cache = await caches.open('pwa-assets');
+        return await cache.match('/offline/');
+      }
+    }
+  })());
 });
