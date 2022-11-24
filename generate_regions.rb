22684a573a6d50d217172cf924a06ddc3c139eb1
Carlgo11 <carlgo11@users.noreply.github.com>
2022-11-24 16:11:45 +0100

    List all regions with >= 10 entries

--- generate_regions.rb
+++ generate_regions.rb
@@ -5,7 +5,7 @@ require 'fileutils'
 require 'json'
 require 'net/http'
 require 'uri'
-
+require 'yaml'
 # Fetch API files
 module API
   def self.fetch(uri)
@@ -21,10 +21,12 @@ end
 categories = JSON.parse(File.read('./data/categories.json'))
 regions = API.fetch('https://2fa.directory/api/v3/regions.json')
 entries = API.fetch('https://2fa.directory/api/v3/all.json')
+used_regions = []
 
 regions.each do |id, region|
-  next unless region['count'] >= 10 || id.eql?('int')
+  next unless region['count'] >= 10 && !id.eql?('int')
 
+  used_regions.push(id)
   dir = "content/#{id}"
   FileUtils.mkdir_p dir
   File.open("#{dir}/_index.md", 'w') do |file|
@@ -33,10 +35,10 @@ regions.each do |id, region|
 
   used_categories = {}
   entries.each do |_, entry|
-    used_regions = entry['regions']
-    unless used_regions.nil?
-      included = used_regions.reject { |r| r.start_with?('-') }
-      excluded = used_regions.select { |r| r.start_with?('-') }.map! { |v| v.tr('-', '') }
+    entry_regions = entry['regions']
+    unless entry_regions.nil?
+      included = entry_regions.reject { |r| r.start_with?('-') }
+      excluded = entry_regions.select { |r| r.start_with?('-') }.map! { |v| v.tr('-', '') }
       next unless included&.include?(id) && !excluded.include?(id)
     end
 
@@ -44,3 +46,16 @@ regions.each do |id, region|
   end
   File.open("./data/#{id}.json", 'w') { |file| file.write JSON.generate used_categories.sort.to_h }
 end
+
+all_regions = {}
+regions_uri = 'https://raw.githubusercontent.com/stefangabos/world_countries/master/data/countries/en/world.json'
+API.fetch(regions_uri).select do |region|
+  all_regions[region['alpha2']] = region['name'] if used_regions.include? region['alpha2']
+end
+# Change long official names
+all_regions.merge!({ 'us' => 'United States', 'gb' => 'United Kingdom', 'tw' => 'Taiwan', 'ru' => 'Russia',
+                     'kr' => 'South Korea' })
+
+# Change output format to array of hashmaps
+output = all_regions.map { |k, v| { 'id' => k, 'name' => v } }
+File.open('data/regions.yml', 'w') { |file| file.write output.to_yaml }
