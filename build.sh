e52004a5594772983b383e53ba2204e80030e30a
Carlgo11 <carlgo11@users.noreply.github.com>
2022-11-27 02:08:38 +0100

    Minify HTML after build

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 #!/usr/bin/env sh
-./generate_entries.rb
-./generate_regions.rb
+# Generate content
+./generate_entries.rb &
+./generate_regions.rb &
+wait
+# Build site
 hugo --minify
+
+# Minify HTML
+for file in public/*/*.html; do
+html-minifier \
+--collapse-whitespace --remove-comments \
+--remove-script-type-attributes --remove-tag-whitespace \
+--use-short-doctype -o "$file" "$file" &
+done
+wait
