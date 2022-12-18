33042eeb2539950387b5fb0c901cfde3c5a97cc5
Carlgo11 <carlgo11@users.noreply.github.com>
2022-12-18 21:03:38 +0100

    Fix #11

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -14,12 +14,17 @@ $('.contact-btn.twitter').click(function (){
   social_media_notice('tweet', $(this).data('lang'), $(this).data('twitter'))
 })
 
+$('#social-media-accept').click(function (){
+  window.localStorage.setItem('social-media-notice', 'hidden');
+  window.open($(this).data('url'), '_blank');
+})
+
 function social_media_notice(type, lang, handle){
   const uri = `/contact/?type=${type}&lang=${lang}&handle=${handle}`
   if (window.localStorage.getItem('social-media-notice') !== 'hidden') {
     let modal = new bootstrap.Modal($('#social-media-warn'));
+    $('#social-media-accept').data('url', uri);
     modal.toggle();
-    $('#social-media-accept').attr('onclick', `window.localStorage.setItem('social-media-notice', 'hidden');window.open('${uri}', '_blank');`)
   } else {
     window.open(uri, '_blank');
   }
