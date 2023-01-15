341f275ce28bcaaf78845115ece444f19f293453
Arun Pattni <20560218+ApCoder123@users.noreply.github.com>
2023-01-15 12:56:19 +0000

    fix excluded regions (#30)

--- scripts/generate_regions.rb
+++ scripts/generate_regions.rb
@@ -36,7 +36,7 @@ regions.each do |id, region|
     unless entry_regions.nil?
       included = entry_regions.reject { |r| r.start_with?('-') }
       excluded = entry_regions.select { |r| r.start_with?('-') }.map! { |v| v.tr('-', '') }
-      next unless included&.include?(id) && !excluded.include?(id)
+      next if (!included.empty? && !included&.include?(id)) || excluded.include?(id)
     end
 
     entry['keywords'].each { |category| used_categories.merge!(categories.slice(category)) }
