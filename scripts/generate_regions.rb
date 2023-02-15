9d537fcf819fea7b5a84be489977ea3de22ea81f
Carlgo11 <carlgo11@users.noreply.github.com>
2023-02-15 02:41:07 +0100

    Simplify generate_regions.rb

--- scripts/generate_regions.rb
+++ scripts/generate_regions.rb
@@ -19,31 +19,24 @@ categories = JSON.parse(File.read('./data/categories.json'))
 regions = API.fetch('https://api.2fa.directory/v3/regions.json')
 entries = API.fetch('https://api.2fa.directory/v3/all.json')
 
-used_regions = []
-regions.each do |id, region|
-  next unless region['count'] >= 10 && !id.eql?('int')
-
-  used_regions.push(id)
-
-  used_categories = {}
-  entries.each do |_, entry|
-    entry_regions = entry['regions']
-    unless entry_regions.nil?
-      included = entry_regions.reject { |r| r.start_with?('-') }
-      excluded = entry_regions.select { |r| r.start_with?('-') }.map! { |v| v.tr('-', '') }
-      next if (!included.empty? && !included&.include?(id)) || excluded.include?(id)
+used_regions = regions.filter_map do |id, region|
+  if region['count'] >= 10 && id != 'int'
+    entries.filter_map do |_, e|
+      e_regions = e['regions']
+      if e_regions.nil? || (e_regions.any? { |r| !r.start_with?('-') && r == id } &&
+        e_regions.none? { |r| r.start_with?('-') && r.tr('-', '') == id })
+        r_categories = e['keywords'].map { |category| categories.slice(category) }.reduce(&:merge)
+        File.write("./data/#{id}.json", JSON.generate(r_categories.sort.to_h))
+        id
+      end
     end
-
-    entry['keywords'].each { |category| used_categories.merge!(categories.slice(category)) }
   end
-  File.open("./data/#{id}.json", 'w') { |file| file.write JSON.generate used_categories.sort.to_h }
-end
+end.flatten.uniq
 
-all_regions = {}
-API.fetch('https://raw.githubusercontent.com/stefangabos/world_countries/master/data/countries/en/world.json')
-   .select do |region|
-  all_regions[region['alpha2']] = region['name'] if used_regions.include? region['alpha2']
-end
+all_regions = API.fetch('https://raw.githubusercontent.com/stefangabos/world_countries/master/data/countries/en/world.json')
+                 .select { |region| used_regions.include?(region['alpha2']) }
+                 .map { |region| [region['alpha2'], region['name']] }
+                 .to_h
 
 # Change long official names
 all_regions.merge!({ 'us' => 'United States',
@@ -56,14 +49,16 @@ all_regions.merge!({ 'us' => 'United States',
 square_flags = %w[ch np va]
 
 # Change output format to array of hashmaps
-output = all_regions.map { |k, v| { 'id' => k, 'name' => v, 'square_flag' => square_flags.include?(k) } }
+output = all_regions.map do |k, v|
+  { 'id' => k, 'name' => v }
+    .tap { |h| h['square_flag'] = 1 if square_flags.include?(k) }
+end
 File.open('data/regions.yml', 'w') { |file| file.write output.to_yaml }
 
 output.each do |region|
   dir = "content/#{region['id']}"
   FileUtils.mkdir_p dir
   File.open("#{dir}/_index.md", 'w') do |file|
-    file.write("---\nlayout: tables\nregion:\n  id: \"#{region['id']}\"\n"\
-               "  name: \"#{region['name']}\"\n  square_flag: #{region['square_flag']}\n---")
+    file.write("#{{ 'layout' => 'tables', 'region' => region }.to_yaml}---")
   end
 end
