b96ff62aaa7434d6c1bf6b83f9be88aef507d43f
Carl <Carlgo11@users.noreply.github.com>
2024-07-29 07:40:02 +0200

    Handle CF Page build errors (#63)

--- functions/redirect.js
+++ functions/redirect.js
@@ -1,13 +1,29 @@
-export async function onRequest({env, request}) {
-  const base = env.base || 'https://2fa.pages.dev/'
-  const country = request.cf?.country.toLowerCase() || "int";
-  let uri = `${base}${country}`
-  const res = await fetch(uri, {
-    cf: {
-      cacheTtl: 86400, cacheEverything: true
-    }
-  })
-  if (res.status !== 200) uri = `${base}/int`
+export async function onRequestGet({request}) {
+  const url = new URL(request.url);
+  const base = `${url.protocol}//${url.hostname}/`;
+  const redirectStatus = 302;
+  try {
+    const country = request.cf?.country?.toLowerCase() || 'int';
+    let uri = `${base}${country}/`;
 
-  return Response.redirect(uri, 302);
+    const res = await fetch(uri, {
+      cf: {
+        cacheTtlByStatus: {
+          '200': 60 * 60 * 24 * 7, // Cache request 1 week
+          '404': 60 * 60 * 24, // Cache request 1 day
+        },
+      },
+    });
+
+    // Redirect to /int/ if that page works
+    if (res.status !== 200) {
+      const int = await fetch(`${base}int/`);
+      uri = int.status === 200 ? `${base}/int/`:`${base}/503/`;
+    }
+
+    return Response.redirect(uri, redirectStatus);
+  } catch (e) {
+    console.error(e);
+    return Response.redirect(`${base}/502/`, redirectStatus);
+  }
 }
