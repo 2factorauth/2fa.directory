2ffb4a8193e9847abb55bc379205c9c95e73028f
H. Kamran <hkamran@hkamran.com>
2024-10-22 23:20:32 -0700

    Add region dropdown

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,38 +1,51 @@
-import { Component, render } from 'preact';
+import { render } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
 
 import "/assets/css/navbar.scss";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
 
-export class Navbar extends Component {
+const API_URL = 'https://api.2fa.directory/frontend/v1';
 
-  render() {
-    const Title = '2FA Directory'
-    return (
-      <nav className="navbar navbar-expand-lg" role="navigation">
-        <div className="container">
-          <a className="nav-link" id="branding" href="">
-            <ul className="icon">
-              <li>{Title}</li>
-            </ul>
+function Navbar() {
+  const [regions, setRegions] = useState({});
+
+  useEffect(() => {
+    fetch(`${API_URL}/regions.json`, { cache: 'force-cache' }).
+      then(res => res.json()).
+      then(data => setRegions(data)).
+      catch(err => console.error('Error fetching regions:', err));  // Add error handling
+  }, []);
+
+  return (
+    <nav className="navbar navbar-expand-lg" role="navigation">
+      <div className="container">
+        {/* TODO: Conditionally handle href */}
+        <a className="nav-link" id="branding" href="/about">
+          <ul className="icon">
+            <li>2FA Directory</li>
+          </ul>
+        </a>
+
+        <a class="nav-link dropdown-toggle" id="regionDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" aria-haspopup="true" aria-label="Choose region" tabindex={0}>
+          <span aria-hidden={true} className={window.location.pathname == "/int/" ? "bi bi-globe" : `fi fi-${window.location.pathname.replace(/\//g, "")}`}></span>
+        </a>
+
+        <div aria-labelledby="regionDropdown" className="dropdown-menu dropdwon-menu-end">
+          <a class="dropdown-item" href="/int/">
+            <span class="fi fi-un"></span>
+            <b>Global</b>
           </a>
-          {/*{{if eq.Layout "tables"}}
-          <a className="nav-link dropdown-toggle" id="regionDropdown"
-             role="button" data-bs-toggle="dropdown" aria-expanded="false"
-             aria-haspopup="true" aria-label="Choose region" tabIndex="0">
-            {{if .Params.Region.id}}
-            <span
-              className="fi fi-{{- .Params.Region.id -}}{{ if .Params.Region.square_flag }} fis{{ end }}"
-              aria-hidden="true"></span>
-            {{ - else -}}
-            <span className="bi bi-globe" aria-hidden="true"></span>
-            {{end}}
-          </a>
-          {{ - partial "regions" .Site.Data.regions -}}
-          {{end}}*/}
+
+          {Object.keys(regions).sort((a, b) => regions[a].name.localeCompare(regions[b].name)).map((region) =>
+            <a class="dropdown-item" href={`/${region}/`}>
+              <span class={`fi fi-${region} ${regions[region].squareFlag && "fis"}`}></span>
+              {regions[region].name}
+            </a>
+          )}
         </div>
-      </nav>
-
-    )
-  }
+      </div>
+    </nav>
+  )
 }
 
 render(<Navbar />, document.getElementById('nav'));
