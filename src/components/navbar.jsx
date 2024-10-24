8dc7afa99414bfe292352fa7674a8879719a0546
H. Kamran <hkamran@hkamran.com>
2024-10-24 14:47:33 -0700

    Show the region dropdown only on directory pages

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,13 @@
-import {render} from 'preact';
-import {useEffect, useState} from 'preact/hooks';
-import {API_URL} from '../constants.js';
+import { render } from 'preact';
+import { useEffect, useState } from 'preact/hooks';
+import { API_URL } from '../constants.js';
 
 import '/node_modules/flag-icons/css/flag-icons.min.css';
 
 function Navbar() {
   const [regions, setRegions] = useState({});
+
+  const region = window.location.pathname.replace(/\//g, "");
 
   useEffect(() => {
     fetch(`${API_URL}/regions.json`, { cache: 'force-cache' }).
@@ -15,18 +17,19 @@ function Navbar() {
   }, []);
 
   return (
-      <div className="container">
-        <a className="nav-link" id="branding" href="https://2factorauth-org.pages.dev/">
-          <ul className="icon">
-            <li>2FA Directory</li>
-          </ul>
-        </a>
+    <div className="container">
+      <a className="nav-link" id="branding" href="https://2factorauth-org.pages.dev/">
+        <ul className="icon">
+          <li>2FA Directory</li>
+        </ul>
+      </a>
 
+      {Object.keys(regions).includes(region) && <>
         <a class="nav-link dropdown-toggle" id="regionDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" aria-haspopup="true" aria-label="Choose region" tabindex={0}>
-          <span aria-hidden={true} className={window.location.pathname == "/int/" ? "bi bi-globe" : `fi fi-${window.location.pathname.replace(/\//g, "")}`}></span>
+          <span aria-hidden={true} className={window.location.pathname == "/int/" ? "bi bi-globe" : `fi fi-${region}`}></span>
         </a>
 
-        <div aria-labelledby="regionDropdown" className="dropdown-menu">
+        <div aria-labelledby="regionDropdown" className="dropdown-menu dropdwon-menu-end">
           <a class="dropdown-item" href="/int/">
             <span class="fi fi-un"></span>
             <b>Global</b>
@@ -39,7 +42,9 @@ function Navbar() {
             </a>
           )}
         </div>
-      </div>
+      </>
+      }
+    </div>
   )
 }
 
