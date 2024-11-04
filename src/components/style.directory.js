228c5ad1d768a65c1bfef10ab1a71d9d3e6803cb
H. Kamran <hkamran@hkamran.com>
2024-11-04 15:35:09 -0800

    Add error pages (#79)
    * Extract directory-only styles
    
    * Add error pages
    
    * Fix redirect function
    
    * Remove 503 error page
    
    ---------
    
    Co-authored-by: Carl <Carlgo11@users.noreply.github.com>

---
+++ src/components/style.directory.js
@@ -0,0 +1,3 @@
+import "/assets/css/search.scss";
+import "/assets/css/category-buttons.scss";
+import "/assets/css/table.scss";
--- src/components/style.js
+++ src/components/style.js
@@ -1,7 +1,4 @@
 import "/assets/css/root.scss";
 import "/assets/css/page.scss";
 import "/assets/css/navbar.scss";
-import "/assets/css/search.scss";
-import "/assets/css/category-buttons.scss";
-import "/assets/css/table.scss";
 import "/assets/css/footer.scss";
